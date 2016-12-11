54be57079fa029bcf49463521cf7c42393dcd663
Matheus Vitório <mvfc@users.noreply.github.com>
2016-12-11 12:41:46 -0200

    Update perceptrongriperesf.py

--- perceptrongriperesf.py
+++ perceptrongriperesf.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+start = time.clock()
+
 w = [
     0,
     0,
@@ -9,11 +11,9 @@ w = [
     0,
 ]
 
-bias = random.random()
+bias = 1
 
 # Saida 1 = gripe, saida 0 = resfriado
-
-# parar gerar entradas aleatorias: entradas = [[random.randint(0, 1) for x in range(5)] for y in range(100)]
 
 entradas = [
     [1, 0, 1, 1, 1],
@@ -24,8 +24,14 @@ entradas = [
     [0, 0, 0, 1, 0],
 ]
 
+entradasreal = [[random.randint(0, 1) for x in range(5)] for y in range(100)]
+
 def teste(i):
     somatorio = bias*w[0]+entradas[i][0]*w[1]+entradas[i][1]*w[2]+entradas[i][2]*w[3]+entradas[i][3]*w[4]
+    return somatorio
+
+def testereal(i):
+    somatorio = bias*w[0]+entradasreal[i][0]*w[1]+entradasreal[i][1]*w[2]+entradasreal[i][2]*w[3]+entradasreal[i][3]*w[4]
     return somatorio
 
 def learning(p, erro):
@@ -33,30 +39,51 @@ def learning(p, erro):
         w[y+1] = w[y+1] + (erro * bias * entradas[p][y])
 flag = 0
 
-nrvezes = input('Numero de iteracoes\n')
-
-for x in range (int(nrvezes)):
-    for p in range(6):
-        tst = teste(p)
-        if(tst > 0):
-            saida = 1
-        elif(tst <= 0):
-            saida = 0
-        print('virus', entradas[p][0])
-        print('bacteria', entradas[p][1])
-        print('dor de cabeca', entradas[p][2])
-        print('corisa', entradas[p][3])
-        if(saida == 1):
-            print('###gripe', saida)
-        elif(saida == 0):
-            print('###resfriado', saida)
-        print('saida esperada', entradas[p][2])
-        print('######################')
-        if(saida != entradas[p][2]):
-            learning(p, (entradas[p][2] - saida))
-            print('Aprendizado!')
-            flag = flag + 1
-        time.sleep(1)
-
-print('!!!!!!!!!!!')
-print('Aprendizado feito', flag, 'vezes')
+for p in range (500):
+    if(flag < 2):
+        for p in range(6):
+            print('####### Aprendizado ######\n')
+            tst = teste(p)
+            if(tst > 0):
+                saida = 1
+            elif(tst <= 0):
+                saida = 0
+            print('virus', entradas[p][0], 'peso 1 ', w[1])
+            print('bacteria', entradas[p][1], 'peso 2 ', w[2])
+            print('dor de cabeca', entradas[p][2], 'peso 3 ', w[3])
+            print('corisa', entradas[p][3], 'peso 4 ', w[4])
+            if(saida == 1):
+                print('###gripe### ', saida)
+            elif(saida == 0):
+                print('###resfriado### ', saida)
+            print('saida esperada', entradas[p][4])
+            print('####### Aprendizado ######\n')
+            if(saida != entradas[p][4]):
+                learning(p, (entradas[p][4] - saida))
+                print('Aprendizado!\n')
+                flag = flag + 1
+            now = time.clock()
+            print('Rodando por ', int(now - start), 'segundos')
+            print('Aprendizado feito', flag, 'vezes')
+            #time.sleep(0.5)
+    else:
+        for p in range(100):
+            print('########## Fase 2 ############\n')
+            tst = testereal(p)
+            if (tst > 0):
+                saida = 1
+            elif (tst <= 0):
+                saida = 0
+            print('virus', entradasreal[p][0], 'peso 1 ', w[1])
+            print('bacteria', entradasreal[p][1], 'peso 2 ', w[2])
+            print('dor de cabeca', entradasreal[p][2], 'peso 3 ', w[3])
+            print('corisa', entradasreal[p][3], 'peso 4 ', w[4])
+            if (saida == 1):
+                print('%%gripe%% ', saida)
+            elif (saida == 0):
+                print('%%resfriado%% ', saida)
+            entradasreal[p][4] = saida
+            print('########## Fase 2 ############\n')
+            now = time.clock()
+            print('Rodando por ', int(now - start), 'segundos')
+            #time.sleep(0.5)
